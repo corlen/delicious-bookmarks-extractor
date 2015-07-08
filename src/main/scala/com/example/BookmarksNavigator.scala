f126ee12ae2c6e3a11ff8bcafe7e472e1d0e3a23
Maximiliano Santiago <maximiliano.santiago@gmail.com>
2015-07-07 22:17:57 -0300

    Final version. It works

--- src/main/scala/com/example/BookmarksNavigator.scala
+++ src/main/scala/com/example/BookmarksNavigator.scala
@@ -3,22 +3,34 @@ package com.example
 import java.io.File
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 
 object BookmarksNavigator {
   def main(args: Array[String]): Unit = {
     val file = new File(getClass.getResource("/delicious.html").getPath)
+    val file2 = new File(getClass.getResource("/delicious-maxsantiago.html").getPath)
+    val file3 = new File(getClass.getResource("/bookmarks_8_11_13.html").getPath)
 
     val htmlFile = Jsoup.parse(file, "UTF-8")
+    val htmlFile2 = Jsoup.parse(file2, "UTF-8")
+    val htmlFile3 = Jsoup.parse(file3, "UTF-8")
 
-    val title = htmlFile.title
-    val aElements = htmlFile.getElementsByTag("a").toArray
+    val links = htmlFile.select("a[href]").toArray
+    val links2 = htmlFile2.select("a[href]").toArray
+    val links3 = htmlFile3.select("a[href]").toArray
+    val linksUnion = links ++ links2 ++ links3
 
-    for(element <- aElements){
-      println("Element: " + element.toString)
+    for(element <- linksUnion){
+      val link = element.asInstanceOf[Element]
+      val linkHref = link.attr("href")
+      val linkText = link.text
+      val linkTags = link.attr("tags")
+
+      println(s"Link href: $linkHref \nLink Text: $linkText\nLink Tags: $linkTags\n")
     }
 
-    println(s"Title: $title and elements quantity: ${aElements.length}")
+    println(s"Elements quantity: ${linksUnion.length}")
 
   }
 }
